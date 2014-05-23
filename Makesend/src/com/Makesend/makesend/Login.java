ac1c80467b71869fba2aa4d7e13f2de7956ae831
nipun90 <nipun90@gmail.com>
2014-05-23 14:47:24 -0400

    Login and Register activity commits

--- Makesend/src/com/Makesend/makesend/Login.java
+++ Makesend/src/com/Makesend/makesend/Login.java
@@ -1,37 +1,32 @@
 package com.Makesend.makesend;
 
-
-
 import android.os.Bundle;
-import android.os.Environment;
 import android.app.Activity;
 import android.content.Intent;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
 
-public class MainActivity extends Activity {
+public class Login extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-		Button Login = (Button)findViewById(R.id.login);
-		Login.setOnClickListener(new Button.OnClickListener(){
+		setContentView(R.layout.activity_login);
+		Button register = (Button)findViewById(R.id.register);
+		register.setOnClickListener(new Button.OnClickListener(){
 
 			@Override
 			public void onClick(View arg0) {
-				Intent myIntent = new Intent(MainActivity.this, Login.class);
-				MainActivity.this.startActivity(myIntent);
+				Intent myIntent = new Intent(Login.this, Register.class);
+				Login.this.startActivity(myIntent);
 			}});
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		
+		getMenuInflater().inflate(R.menu.login, menu);
 		return true;
 	}
 
