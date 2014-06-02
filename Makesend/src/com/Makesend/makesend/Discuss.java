c9cc7fc78c23014ae04d2dd946deb6ef1ce37859
nipun90 <nipun90@gmail.com>
2014-06-02 12:13:26 -0400

    Commit for optional and discuss activities

---
+++ Makesend/src/com/Makesend/makesend/Discuss.java
@@ -0,0 +1,22 @@
+package com.Makesend.makesend;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class Discuss extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_discuss);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.discuss, menu);
+		return true;
+	}
+
+}
--- Makesend/src/com/Makesend/makesend/Optional.java
+++ Makesend/src/com/Makesend/makesend/Optional.java
@@ -7,26 +7,27 @@ import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 
-public class Register extends Activity {
+public class Optional extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_register);
-		Button register = (Button)findViewById(R.id.reg);
-		register.setOnClickListener(new Button.OnClickListener(){
+		setContentView(R.layout.activity_optional);
+		Button skip = (Button)findViewById(R.id.skip);
+		skip.setOnClickListener(new Button.OnClickListener(){
 
 			@Override
 			public void onClick(View arg0) {
-				Intent myIntent = new Intent(Register.this, Optional.class);
-				Register.this.startActivity(myIntent);
+				Intent myIntent = new Intent(Optional.this, Discuss.class);
+				Optional.this.startActivity(myIntent);
 			}});
+		
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.register, menu);
+		getMenuInflater().inflate(R.menu.optional, menu);
 		return true;
 	}
 
